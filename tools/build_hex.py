37af8fc7e1891812b41f6f4708de4f24060aaf17
Oleg Chagin <oleg.c@ks2corp.com>
2025-02-24 05:44:38 +0300

    modified:   tools/build_hex.py

--- tools/build_hex.py
+++ tools/build_hex.py
@@ -1,20 +1,12 @@
 Import("env")
 import os
 
-# Получаем путь к директории сборки
 build_dir = env.subst("$BUILD_DIR").rstrip(os.sep)
-print(f"Путь к директории сборки: {build_dir}")
-
-# Извлекаем имя последней директории из пути
 build_name = os.path.basename(build_dir)
-print(f"Имя последней директории (среды сборки): {build_name}")
-
-# Генерация .hex файла из .elf
 env.AddPostAction(
     "$BUILD_DIR/${PROGNAME}.elf",
     env.VerboseAction(
-        # Используем env.subst для подстановки переменных в команду
         env.subst("$OBJCOPY -O ihex $BUILD_DIR/${PROGNAME}.elf $BUILD_DIR/" + build_name + ".hex"),
-        f"Создание {build_dir}/{build_name}.hex"
+        f"Creating {build_dir}/{build_name}.hex"
     )
 )
